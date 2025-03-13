4fee20851bb78cdd947feee0b7fce5c0c208c9dc
Grasieli Wiedthauger <grasieli.wie@hotmail.com>
2025-03-12 21:24:13 -0300

    Lista1

---
+++ Lista 1/Lista1.cpp
@@ -0,0 +1,67 @@
+/*
+
+QUESTÃO 1:
+
+                linguagem C:
+                        #include <stdio.h>
+
+                            int main()
+                            {
+                                printf("Hello World");
+
+                            return 0;
+                            }
+
+                linguagem GO:
+                        package main
+                        import "fmt"
+
+                        func main() {
+                            fmt.Println("Hello World")
+                        }
+                linguagem python:
+                        print ('Hello World')
+
+QUESTÃO 2: Um programa deve calcular a idade de uma pessoa com base no ano de nascimento fornecido pelo
+usuário. O programa exibe a idade calculada na tela. 
+
+a) Qual é a entrada de dados? Ano de nascimento fornecido pelo usuário
+b) Qual é o processamento realizado? O programa armazena ao ano fornecido pelo usuario, faz o calculo para saber a idade e
+armazena o resultado em uma variavel
+c) Qual é a saída de dados? Idade calculada pelo programa baseada no ano fornecido pelo usuario
+
+
+QUESTÃO 3:
+• Entrada de dados (informações fornecidas pelo usuário)
+• Saída de dados (informação que será exibida para o usuário)
+• Possíveis variáveis que poderiam ser utilizadas para armazenar os dados e realizar o
+processamento.
+
+A)  Entrada de dados: valor do salário base e o percentual de bônus aplicado
+    Saída de dados: salario final de um funcionario
+    Variaveis: salarioBase, aplicadoBonus, salarioFinal - todas em float
+
+B)  Entrada de dados: quantos litros abasteceu e o preço por litro
+    Saída de dados: valor a ser pago por um cliente
+    Variaveis: litrosAbastecidos (float), precoLitro (float), valorTotal(float)
+
+C)  Entrada de dados: sexo (M ou F), dia, mês e ano de nascimento, altura (em metros) e peso (em kg).
+    Saída de dados: informações básicas dos pacientes para futuras consultas
+    Variaveis: sexo (char), diaNascimento (int), mesNascimento (int), ano Nascimento (int), altura (float), peso (float)
+
+QUESTÃO 4:
+
+A) precoFinal = (valorOriginal*1,1) - 5
+
+B) mediaPonderada = ((n1*p1)+(n2*p2)+(n3*p3))/(p1+p2+p3)
+
+C) distancia_total = (tempo1 * velocidade1) + (tempo2 * velocidade3)
+
+D) y = a * x*x + b*x + c*x
+
+*/
+
+
+
+
+
