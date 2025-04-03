51ecf5a94137d41011670d5071bd3e35aeb2340d
Grasieli Wiedthauger <grasieli.wie@hotmail.com>
2025-04-02 21:48:47 -0300

    Ex9L3 pt1

---
+++ Lista 3/Ex9L3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main (){
+
+float cotacaoDolar, cotacaoEuro;
+int menu;
+
+printf("Digite a cotacao do dolar:");
+scanf(" %d", &cotacaoDolar);
+printf("Digite a cotacao do euro:");
+scanf(" %d", &cotacaoEuro);
+
+switch (menu)
+{
+case 1:         //real para euro
+    /* code */
+    break;
+
+case 2:         //real para dolar
+    /* code */
+    break;
+
+case 3:         //euro para dolar
+    /* code */
+    break;
+
+case 4:         //euro para real
+    /* code */
+    break;
+
+case 5:         //dolar para euro
+    /* code */
+    break;
+
+case 6:         //dolar para real
+    /* code */
+    break;
+
+default:
+    break;
+}
+
+    return 0;
+}
