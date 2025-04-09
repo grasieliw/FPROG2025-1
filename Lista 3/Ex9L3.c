2aef1eaa23df61087160c72c767a53ad9a465cdd
Grasieli Wiedthauger <grasieli.wie@hotmail.com>
2025-04-09 20:19:25 -0300

    Ex9L3 pt2

--- Lista 3/Ex9L3.c
+++ Lista 3/Ex9L3.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
 
-int main (){
+int main() {
+    float cotacaoDolar, cotacaoEuro;
+    int menu;
 
-float cotacaoDolar, cotacaoEuro;
-int menu;
+    printf("Digite a cotacao do dolar: ");
+    scanf(" %f", &cotacaoDolar);
 
-printf("Digite a cotacao do dolar:");
-scanf(" %d", &cotacaoDolar);
-printf("Digite a cotacao do euro:");
-scanf(" %d", &cotacaoEuro);
+    printf("Digite a cotacao do euro: ");
+    scanf(" %f", &cotacaoEuro);
 
-switch (menu)
-{
-case 1:         //real para euro
-    /* code */
-    break;
+    printf("Digite um numero:\n 1 - Real para Euro\n 2 - Real para Dolar\n 3 - Euro para Dolar\n 4 - Euro para Real\n 5 - Dolar para Euro\n 6 - Dolar para Real\n");
+    scanf("%d", &menu);
 
-case 2:         //real para dolar
-    /* code */
-    break;
+    printf("Opcao escolhida: %f\n", cotacaoEuro);
 
-case 3:         //euro para dolar
-    /* code */
-    break;
-
-case 4:         //euro para real
-    /* code */
-    break;
-
-case 5:         //dolar para euro
-    /* code */
-    break;
-
-case 6:         //dolar para real
-    /* code */
-    break;
-
-default:
-    break;
-}
+    switch (menu) {
+        case 1:  // real para euro
+            printf("Cotacao de real para euro: %.2f \n", cotacaoEuro);
+            break;
+        case 2:  // real para dolar
+            printf("Cotacao de real para dolar: %.2f\n", cotacaoDolar);
+            break;
+        case 3:  // euro para dolar
+            printf("Cotacao de euro para dolar: %.2f\n", (cotacaoEuro / cotacaoDolar));
+            break;
+        case 4:  // euro para real
+            printf("Cotacao de euro para real: %.2f\n", (1 / cotacaoEuro));
+            break;
+        case 5:  // dolar para euro
+            printf("Cotacao de dolar para euro: %.2f\n", (cotacaoDolar / cotacaoEuro));
+            break;
+        case 6:  // dolar para real
+            printf("Cotacao de dolar para real: %.2f\n", (1 / cotacaoDolar));
+            break;
+        default:
+            printf("Opção inválida.\n");
+            break;
+    }
 
     return 0;
 }
